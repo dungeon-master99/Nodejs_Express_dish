7072dbd7ba0134b20c687d533d43fd25e7b7b850
dungeon-master99 <vaibhavkumarraj99@gmail.com>
2020-05-10 02:56:31 +0530

    implemented leaderRouter and promoRouter

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
 const dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -10,35 +13,19 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+
 app.use('/dishes', dishRouter);
-
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-  res.write('Updating the dish: ' + req.params.dishId + '\n');
-  res.end('Will update the dish: ' + req.body.name + 
-        ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-});
+});*/
 
 const server = http.createServer(app);
 
---
+++ routes/leaderRouter.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.all( (req, res, next) => {
+   res.writeHead(200, {'Content-Type':'text/plain'});
+   next();
+})
+.get( (req, res, next) => {
+   res.end('Will get all the leaders to you!');
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /leader');
+})
+.post( (req, res, next) => {
+   res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.delete( (req, res, next) => {
+   res.end('Deleting all leaders!')
+});
+
+leaderRouter.route('/:leaderId')
+.all( (req, res, next) => {
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    next();
+})
+.get( (req, res, next) => {
+    res.end('Will send details of the leader ' + req.params.leaderId + ' to you!')
+})
+.put( (req, res, next) => {
+    res.write('Updating the leader: ' + req.params.leaderId + '\n');
+    res.end('Will update the leader: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /leader/' + req.params.leaderId);
+})
+.delete( (req, res, next) => {
+    res.end('Deleting leader: ' + req.params.leaderId);
+});
+
+module.exports = leaderRouter;
